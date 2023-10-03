a81f34d10b89e409280b83df0d2590baf8e218a5
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-03 13:02:20 +0100

    Exercise 4.5 Check if number has square root

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise4._5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #nullable disable
+        string message = "";
+        
+        Console.WriteLine("Welcome to Exercise 4.5 Pre Learning!");
+        Console.WriteLine("      Is number Square Root?");
+        Console.WriteLine("=====================================");
+
+        Console.WriteLine();
+        Console.Write("Please type an integer: ");
+        int number = int.Parse(Console.ReadLine());
+        message = String.Format("No square root for {0}", number);
+        
+        for (int i = 0; i < number; i++)
+        {
+            if(i * i == number){
+                message = String.Format("The square root of {0} is {1}", number, i);
+                break;
+            }
+        } 
+        Console.WriteLine(message);
+    }
+}
